a88832862ee27ebb8027ba1f9f086bd0f84bf125
Martynas Bielskus <61162271+martis99@users.noreply.github.com>
2023-02-15 18:09:37 +0200

    rename drivers, libs and packages folders

--- drivers/gfx/gfx/src/ctx/ctx_driver.h
+++ drivers/gfx/gfx/src/ctx/ctx_driver.h
@@ -11,7 +11,7 @@ CtxDriver *ctx_driver_get_by_id(int id);
 const char *ctx_driver_get_name(int id);
 
 #define CTX_DRIVER(_name, _driver)               \
-	DRIVER(ctx_drtiver_##_name)              \
+	DRIVER(ctx_driver_##_name)               \
 	{                                        \
 		ctx_driver_add(#_name, _driver); \
 	}
--- drivers/gfx/gfx/src/gfx/gfx_driver.h
+++ drivers/gfx/gfx/src/gfx/gfx_driver.h
@@ -11,7 +11,7 @@ GfxDriver *gfx_driver_get_by_id(int id);
 const char *gfx_driver_get_name(int id);
 
 #define GFX_DRIVER(_name, _driver)               \
-	DRIVER(gfx_drtiver_##_name)              \
+	DRIVER(gfx_driver_##_name)               \
 	{                                        \
 		gfx_driver_add(#_name, _driver); \
 	}
